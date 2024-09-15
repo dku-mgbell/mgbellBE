828378d83e9e176b6d7d87e75c6b317677e31f7c
Sermadl <rlatpdms1014@gmail.com>
2024-09-15 16:58:10 +0900

    📰Init project

---
+++ mgbell/src/test/java/com/mgbell/MgbellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgbell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgbellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
