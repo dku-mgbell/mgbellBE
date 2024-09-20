3485b76a371017ecca0dedc4fceb3443de1e1f0d
Sermadl <rlatpdms1014@gmail.com>
2024-09-20 18:09:09 +0900

    Feature: Jwt 필터 사용을 위한 SecurityConfig 수정

--- mgbell/src/main/java/com/mgbell/global/config/SecurityConfig.java
+++ mgbell/src/main/java/com/mgbell/global/config/SecurityConfig.java
@@ -1,11 +1,20 @@
 package com.mgbell.global.config;
 
+import com.mgbell.global.auth.jwt.JwtAuthEntryPoint;
+import com.mgbell.global.auth.jwt.JwtAuthenticationFilter;
+import com.mgbell.global.auth.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -15,7 +24,22 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtProvider jwtProvider;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtProvider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
@@ -48,6 +72,7 @@ public class SecurityConfig {
                             "/webjars/**").permitAll();
                     authorizeRequests.requestMatchers("/user/**").permitAll();
                 })
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
