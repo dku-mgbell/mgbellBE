ee37ea751800a33ee3e161bca4e92baeac0f0566
Sermadl <rlatpdms1014@gmail.com>
2024-09-20 18:08:39 +0900

    Feature: 로그인 DTO 생성

---
+++ mgbell/src/main/java/com/mgbell/user/model/dto/response/LoginResponse.java
@@ -0,0 +1,11 @@
+package com.mgbell.user.model.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+    private String accessToken;
+    private String refreshToken;
+}
