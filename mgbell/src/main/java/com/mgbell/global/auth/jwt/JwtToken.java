da730c78662b4f9738af5f5bb64cd3eba6badce5
Sermadl <rlatpdms1014@gmail.com>
2024-09-20 18:09:43 +0900

    Feature: JwtToken 클래스에 @Getter 추가

--- mgbell/src/main/java/com/mgbell/global/auth/jwt/JwtToken.java
+++ mgbell/src/main/java/com/mgbell/global/auth/jwt/JwtToken.java
@@ -2,7 +2,9 @@ package com.mgbell.global.auth.jwt;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 @AllArgsConstructor
 public class JwtToken {
