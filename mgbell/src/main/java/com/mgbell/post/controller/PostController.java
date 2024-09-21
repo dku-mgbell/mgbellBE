a7b5a28696a2e3329b9cfd71d896c01867b6004a
Sermadl <rlatpdms1014@gmail.com>
2024-09-21 22:47:27 +0900

    Feature: 판매글 생성/조회 API 등록

---
+++ mgbell/src/main/java/com/mgbell/post/controller/PostController.java
@@ -0,0 +1,35 @@
+package com.mgbell.post.controller;
+
+import com.mgbell.post.model.dto.request.PostCreateRequest;
+import com.mgbell.post.model.dto.response.PostPreviewResponse;
+import com.mgbell.post.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/post")
+@AllArgsConstructor
+public class PostController {
+    private final PostService postService;
+
+    @GetMapping("/list")
+    public ResponseEntity<Page<PostPreviewResponse>> list(Pageable pageable) {
+        Page<PostPreviewResponse> posts = postService.showAllPost(pageable);
+
+        return ResponseEntity.ok(posts);
+    }
+
+    @PostMapping
+    @Operation(summary = "게시글 생성")
+    public void create(@RequestBody @Validated PostCreateRequest request) {
+        postService.create(request);
+    }
+
+}
