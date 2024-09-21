c1b5b7f8ddc830d55314e1e54b328349c1e257ae
Sermadl <rlatpdms1014@gmail.com>
2024-09-21 22:47:43 +0900

    Feature: 판매글 생성 request

---
+++ mgbell/src/main/java/com/mgbell/post/model/dto/request/PostCreateRequest.java
@@ -0,0 +1,30 @@
+package com.mgbell.post.model.dto.request;
+
+import com.mgbell.post.model.entity.Cost;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+
+@Getter
+@AllArgsConstructor
+public class PostCreateRequest {
+    @NotNull(message = "userId error")
+    private Long userId;
+    @NotBlank(message = "title error")
+    private String title;
+    @NotBlank(message = "content error")
+    private String content;
+    @NotNull(message = "cost error")
+    private Cost cost;
+    @NotNull(message = "amount")
+    private int amount;
+
+    @NotNull(message = "pickupTime error")
+    @Valid
+    private ArrayList<PickupTimeCreateRequest> pickupTime;
+
+}
