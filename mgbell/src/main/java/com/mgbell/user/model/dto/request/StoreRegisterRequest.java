93ed799f3a6b736c74de93b3578c3942566e361e
Sermadl <rlatpdms1014@gmail.com>
2024-09-30 16:43:03 +0900

    Feature: 로그인 한 계정 권한 검증 후 API 사용할 수 있음

--- mgbell/src/main/java/com/mgbell/user/model/dto/request/StoreRegisterRequest.java
+++ mgbell/src/main/java/com/mgbell/user/model/dto/request/StoreRegisterRequest.java
@@ -4,14 +4,12 @@ import com.mgbell.user.model.entity.store.StoreType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class StoreRegisterRequest {
-    private Long userId;
     private String name;
     private String address;
     private StoreType storeType;
