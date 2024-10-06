85cc7dd0ec3422954602ce6188a9ab11cc6f10a3
Sermadl <rlatpdms1014@gmail.com>
2024-10-06 14:46:41 +0900

    Feature: 스웨거 cors 에러 해결

--- mgbell/src/main/java/com/mgbell/global/config/swagger/SwaggerConfig.java
+++ mgbell/src/main/java/com/mgbell/global/config/swagger/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://15.165.235.145.nip.io", description = "https 서버"),
+                @Server(url = "http://localhost:8080", description = "local 서버"),
+                @Server(url = "http://15.165.235.145:8080", description = "http 서버")
+        },
         info = @Info(title = "Mgbell App", version = "v01"))
 @SecurityScheme(
         name = "JWT Token",
