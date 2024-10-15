430b24c88476f1d37419ff562fac5e0d0e78b9be
Sermadl <rlatpdms1014@gmail.com>
2024-10-16 02:15:36 +0900

    Feature: 주문 기능 추가에 따른 Order 엔티티 추가

---
+++ mgbell/src/main/java/com/mgbell/order/model/entity/Order.java
@@ -0,0 +1,44 @@
+package com.mgbell.order.model.entity;
+
+import com.mgbell.global.util.BaseEntity;
+import com.mgbell.store.model.entity.Store;
+import com.mgbell.user.model.entity.user.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "order_history")
+public class Order extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Setter
+    @Enumerated(EnumType.STRING)
+    private OrderState state;
+    private String request;
+    @NotNull
+    private LocalTime pickupTime;
+    @NotNull
+    private int amount;
+    @NotNull
+    private int subtotal;
+    @Enumerated(EnumType.STRING)
+    private Payment payment;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @NotNull
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+}
