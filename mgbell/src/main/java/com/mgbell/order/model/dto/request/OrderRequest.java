8e06721e8c7b7b815d64be2b9eed634cd231cee5
Sermadl <rlatpdms1014@gmail.com>
2024-10-16 02:38:26 +0900

    Feature: Order Request DTO 추가

---
+++ mgbell/src/main/java/com/mgbell/order/model/dto/request/OrderRequest.java
@@ -0,0 +1,25 @@
+package com.mgbell.order.model.dto.request;
+
+import com.mgbell.order.model.entity.Payment;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    @NotNull(message = "store id error")
+    private Long storeId;
+    @NotNull(message = "pick up time error")
+    @DateTimeFormat(pattern = "HH:mm")
+    private LocalTime pickupTime;
+    private String request;
+    private Payment payment = Payment.SPOT;
+    @NotNull(message = "amount error")
+    private int amount;
+}
