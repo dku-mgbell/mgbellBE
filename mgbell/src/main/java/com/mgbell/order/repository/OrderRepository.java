cf6039ead63c76b6b4433dea27a35d7532aab4e5
Sermadl <rlatpdms1014@gmail.com>
2024-10-16 02:38:12 +0900

    Feature: Order Repository 추가

---
+++ mgbell/src/main/java/com/mgbell/order/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.mgbell.order.repository;
+
+import com.mgbell.order.model.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    Page<Order> findByUserId(Pageable pageable, Long userId);
+}
