81af1d2b50b60b4628a0a5d1312526d697f099d6
Sermadl <rlatpdms1014@gmail.com>
2024-10-16 02:37:13 +0900

    Feature: 가게로 게시글 찾기 기능 추가

--- mgbell/src/main/java/com/mgbell/post/repository/PostRepository.java
+++ mgbell/src/main/java/com/mgbell/post/repository/PostRepository.java
@@ -1,10 +1,12 @@
 package com.mgbell.post.repository;
 
 import com.mgbell.post.model.entity.Post;
+import com.mgbell.store.model.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long>{
     Optional<Post> findByUserId(Long id);
+    Optional<Post> findByStore(Store store);
 }
