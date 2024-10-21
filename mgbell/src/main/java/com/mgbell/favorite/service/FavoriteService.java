97939a59fc35ab26fa4d1115efa2674284354174
Sermadl <rlatpdms1014@gmail.com>
2024-10-22 02:21:37 +0900

    Feature: 즐겨찾기 리스트 보기 기능 추가

--- mgbell/src/main/java/com/mgbell/favorite/service/FavoriteService.java
+++ mgbell/src/main/java/com/mgbell/favorite/service/FavoriteService.java
@@ -2,8 +2,10 @@ package com.mgbell.favorite.service;
 
 import com.mgbell.favorite.exception.FavoriteNotExist;
 import com.mgbell.favorite.model.dto.request.FavoriteRequest;
+import com.mgbell.favorite.model.dto.response.FavoriteResponse;
 import com.mgbell.favorite.model.entity.Favorite;
 import com.mgbell.favorite.repository.FavoriteRepository;
+import com.mgbell.post.model.entity.Post;
 import com.mgbell.store.exception.StoreNotFoundException;
 import com.mgbell.store.model.entity.Store;
 import com.mgbell.store.repository.StoreRepository;
@@ -13,7 +15,11 @@ import com.mgbell.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @Service
@@ -46,5 +52,42 @@ public class FavoriteService {
             store.decreaseFavorited();
             favoriteRepository.delete(favorite);
         }
+    }
+
+    public Page<FavoriteResponse> getFavoriteList(Pageable pageable, Long userId) {
+        Page<Favorite> favorites = favoriteRepository.findByUserId(pageable, userId);
+
+        return getFavoriteResponses(favorites);
+    }
+
+    private Page<FavoriteResponse> getFavoriteResponses(Page<Favorite> favorites) {
+        return favorites.map(currFavorite -> {
+            Store store = currFavorite.getStore();
+            Post post = store.getPost();
+//            List<PostFileResponse> files = currPost.getFiles().stream()
+//                    .map(file -> {
+//                        String url = fileUploadService.getFileUrl(file.getFileId());
+//                        return new PostFileResponse(file, url);
+//                    }).collect(Collectors.toList());
+            //            List<PostFileResponse> files = currPost.getFiles().stream()
+//                    .map(file -> {
+//                        String url = fileUploadService.getFileUrl(file.getFileId());
+//                        return new PostFileResponse(file, url);
+//                    }).collect(Collectors.toList());
+
+            return new FavoriteResponse(
+                    store.getPost().getPostId(),
+                    store.getStoreName(),
+                    post.getBagName(),
+                    post.isOnSale(),
+                    post.getStartAt().format(DateTimeFormatter.ofPattern("HH:mm")),
+                    post.getEndAt().format(DateTimeFormatter.ofPattern("HH:mm")),
+                    store.getAddress(),
+                    store.getLongitude(),
+                    store.getLatitude(),
+                    post.getCostPrice(),
+                    post.getSalePrice(),
+                    post.getAmount());
+        });
     }
 }
