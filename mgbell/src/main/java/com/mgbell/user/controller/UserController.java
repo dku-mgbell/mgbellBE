4cffb729696702201193e48e1738b2e8f0ccb235
Sermadl <rlatpdms1014@gmail.com>
2024-10-21 19:48:31 +0900

    Feature: Swagger API 설명 추가

--- mgbell/src/main/java/com/mgbell/user/controller/UserController.java
+++ mgbell/src/main/java/com/mgbell/user/controller/UserController.java
@@ -19,35 +19,40 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping(path = "/login")
+    @Operation(summary = "로그인")
     public ResponseEntity<LoginResponse> login(@RequestBody @Validated LoginRequest request) {
         return ResponseEntity.ok(userService.login(request));
     }
 
     @AllUserAuth
     @PatchMapping(path = "/edit")
+    @Operation(summary = "회원 정보 수정")
     public void edit(@RequestBody UserEditRequest request, JwtAuthentication auth) {
         userService.edit(request, auth.getUserId());
     }
 
     @AllUserAuth
     @DeleteMapping(path = "/delete")
+    @Operation(summary = "회원 탈퇴")
     public void delete(JwtAuthentication auth) {
         userService.delete(auth.getUserId());
     }
 
     @AllUserAuth
     @PatchMapping(path = "/password")
+    @Operation(summary = "비밀번호 재설정")
     public void updatePwd(@RequestBody PasswordUpdateRequest request, JwtAuthentication auth) {
         userService.updatePwd(request, auth.getUserId());
     }
 
     @PostMapping(path = "/signup")
+    @Operation(summary = "회원가입")
     public void signup(@RequestBody @Validated SignupRequest request) {
         userService.signUp(request);
     }
 
     @PostMapping(path = "/dupCheck")
-    @Operation(summary = "중복 검사 결과 반환(True: 중복됨, False: 중복 되지 않음)")
+    @Operation(summary = "아이디 중복 검사 결과 반환(True: 중복됨, False: 중복 되지 않음)")
     public ResponseEntity<IdDupValidResponse> dupCheck(@RequestBody @Validated EmailRequest request) {
         boolean valid = userService.isDuplicateEmail(request.getEmail());
         IdDupValidResponse response = IdDupValidResponse.builder()
