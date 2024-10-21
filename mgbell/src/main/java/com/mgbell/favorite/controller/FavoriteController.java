e6782ffb6ceb57665c1f09c5d610e02ef7f1a37e
Sermadl <rlatpdms1014@gmail.com>
2024-10-22 02:22:48 +0900

    Feature: Favorite Controller에 즐겨찾기 리스트 보기 API 추가

--- mgbell/src/main/java/com/mgbell/favorite/controller/FavoriteController.java
+++ mgbell/src/main/java/com/mgbell/favorite/controller/FavoriteController.java
@@ -1,11 +1,14 @@
 package com.mgbell.favorite.controller;
 
 import com.mgbell.favorite.model.dto.request.FavoriteRequest;
-import com.mgbell.favorite.model.entity.Favorite;
+import com.mgbell.favorite.model.dto.response.FavoriteResponse;
 import com.mgbell.favorite.service.FavoriteService;
 import com.mgbell.global.auth.jwt.JwtAuthentication;
 import com.mgbell.global.config.swagger.UserAuth;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,5 +21,11 @@ public class FavoriteController {
     @PostMapping
     public void favoriteUpdate(@RequestBody FavoriteRequest request, JwtAuthentication auth) {
         favoriteService.favoriteUpdate(request, auth.getUserId());
+    }
+
+    @UserAuth
+    @GetMapping
+    public ResponseEntity<Page<FavoriteResponse>> getFavorites(Pageable pageable, JwtAuthentication auth) {
+        return ResponseEntity.ok(favoriteService.getFavoriteList(pageable, auth.getUserId()));
     }
 }
