ad657e1bd8c83ab2785d5553ac9987d109eebf71
Sermadl <rlatpdms1014@gmail.com>
2024-10-21 19:44:54 +0900

    Feature: Store Entity 변경에 따른 Request DTO 변경

--- mgbell/src/main/java/com/mgbell/store/model/dto/request/StoreRegisterRequest.java
+++ mgbell/src/main/java/com/mgbell/store/model/dto/request/StoreRegisterRequest.java
@@ -1,6 +1,7 @@
 package com.mgbell.store.model.dto.request;
 
 import com.mgbell.store.model.entity.StoreType;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class StoreRegisterRequest {
-    private String name;
+    @NotNull
+    private String storeName;
+    @NotNull
+    private String ownerName;
+    @NotNull
+    private String contact;
+    @NotNull
+    private String businessRegiNum;
+    @NotNull
     private String address;
     private String longitude;
     private String latitude;
+    @NotNull
     private StoreType storeType;
 //    private MultipartFile image;
 }
