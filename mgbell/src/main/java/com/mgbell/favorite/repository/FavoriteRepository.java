d5beed6c667f09334333a498cbed59dbb19558b3
Sermadl <rlatpdms1014@gmail.com>
2024-10-22 02:22:25 +0900

    Feature: Favorite Repository UserId로 Favorite 찾기 기능 추가

--- mgbell/src/main/java/com/mgbell/favorite/repository/FavoriteRepository.java
+++ mgbell/src/main/java/com/mgbell/favorite/repository/FavoriteRepository.java
@@ -1,6 +1,8 @@
 package com.mgbell.favorite.repository;
 
 import com.mgbell.favorite.model.entity.Favorite;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +10,5 @@ import java.util.Optional;
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     boolean existsByStoreIdAndUserId(Long storeId, Long userId);
     Optional<Favorite> findByStoreIdAndUserId(Long storeId, Long userId);
+    Page<Favorite> findByUserId(Pageable pageable, Long userId);
 }
