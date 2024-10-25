4f37f747dd0f31d251b539620c9a29cd681d7ad7
Sermadl <rlatpdms1014@gmail.com>
2024-10-25 21:45:01 +0900

    Feature: 예외처리

---
+++ mgbell/src/main/java/com/mgbell/global/s3/exception/FailedToLoadImage.java
@@ -0,0 +1,7 @@
+package com.mgbell.global.s3.exception;
+
+public class FailedToLoadImage extends RuntimeException {
+    public FailedToLoadImage() {
+        super("FAILED TO LOAD IMAGE");
+    }
+}
