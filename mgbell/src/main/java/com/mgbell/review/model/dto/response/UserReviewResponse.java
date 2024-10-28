b15077044b11438ff91f8edcf677e484d414a42a
Sermadl <rlatpdms1014@gmail.com>
2024-10-29 04:03:17 +0900

    Feature: 일반 사용자 내가 쓴 리뷰 조회 DTO 추가

---
+++ mgbell/src/main/java/com/mgbell/review/model/dto/response/UserReviewResponse.java
@@ -0,0 +1,26 @@
+package com.mgbell.review.model.dto.response;
+
+import com.mgbell.review.model.entity.ReviewScore;
+import com.mgbell.review.model.entity.SatisfiedReason;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserReviewResponse {
+    private Long storeId;
+    private Long reviewId;
+    private LocalDateTime createdAt;
+    private String storeName;
+    private ReviewScore reviewScore;
+    private String content;
+    private String ownerComent;
+    private List<SatisfiedReason> satisfiedReasons = new ArrayList<>();
+    private List<String> images = new ArrayList<>();
+}
