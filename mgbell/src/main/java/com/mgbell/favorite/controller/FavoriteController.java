d2ba093c8c79e1ff872efc0ccbdcb5f284cb1f56
Sermadl <rlatpdms1014@gmail.com>
2024-10-29 04:09:14 +0900

    Feature: Swagger 설명 추가

--- mgbell/src/main/java/com/mgbell/favorite/controller/FavoriteController.java
+++ mgbell/src/main/java/com/mgbell/favorite/controller/FavoriteController.java
@@ -5,6 +5,7 @@ import com.mgbell.favorite.model.dto.response.FavoriteResponse;
 import com.mgbell.favorite.service.FavoriteService;
 import com.mgbell.global.auth.jwt.JwtAuthentication;
 import com.mgbell.global.config.swagger.UserAuth;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,12 +20,14 @@ public class FavoriteController {
 
     @UserAuth
     @PostMapping
+    @Operation(summary = "일반 사용자 API: 즐겨찾기 추가/제거하기")
     public void favoriteUpdate(@RequestBody FavoriteRequest request, JwtAuthentication auth) {
         favoriteService.favoriteUpdate(request, auth.getUserId());
     }
 
     @UserAuth
     @GetMapping
+    @Operation(summary = "일반 사용자 API: 즐겨찾기 목록 보기")
     public ResponseEntity<Page<FavoriteResponse>> getFavorites(Pageable pageable, JwtAuthentication auth) {
         return ResponseEntity.ok(favoriteService.getFavoriteList(pageable, auth.getUserId()));
     }
